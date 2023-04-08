dfccacd1a5d124e1c87a31f0f39e1046bd9d6ef2
Leandro Cruz <LeandroCruz717@outlook.com>
2023-04-08 14:26:23 -0300

    feat: adicionar docker e docker-compose

---
+++ public/login/index.js
@@ -0,0 +1,51 @@
+// fazer requisição de login
+async function makeLoginRequest(usernameOrEmail, password) {
+    const response = await fetch(SERVER + "auth/signin", {
+        body: JSON.stringify({
+            usernameOrEmail,
+            password
+        }),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        credentials: "include"
+    });
+
+    if (!response.ok) {
+        try {
+            const { code, message, errors } = await response.json();
+            const [messageName, messageText] = message.split("|");
+
+            return handleResponseError(messageName, messageText);
+        }
+        catch (err) {
+
+            throw new Error("Não foi possível completar o login");
+        }
+    }
+
+    return await response.json();
+}
+
+// obtém campos do formulário
+function getFormData() {
+    const usernameOrEmail = document.getElementById("usernameOrEmail").value;
+    const password = document.getElementById("password").value;
+
+    return {
+        usernameOrEmail,
+        password
+    }
+}
+
+document.querySelector("form").onsubmit = async function (e) {
+    e.preventDefault();
+
+    const { password, usernameOrEmail } = getFormData();
+    const data = await makeLoginRequest(usernameOrEmail, password);
+
+    // receebe a resposta arqui
+    console.log({ data })
+}
