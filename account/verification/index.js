17615c432cca595777515929047d04709b5c81b2
iamyan <92941649+yanrodrigues205@users.noreply.github.com>
2023-08-23 20:12:42 +0000

    ajustes página de verificação

--- account/verification/index.js
+++ account/verification/index.js
@@ -1,29 +1,59 @@
-const inputs = [...document.querySelectorAll("#verification")];
 
-inputs.forEach((input, index, arr) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = [...document.querySelectorAll("#verification")];
+    const btnEnviar = document.getElementById("btn-enviar");
 
-    input.addEventListener("input", () =>{
-        if(index !== arr.length - 1){
-            inputs[index + 1 ].focus();
-        }
+    btnEnviar.addEventListener("click", () => {
+        verifyAccount();
     });
-    
-    
-    input.addEventListener("keyup", (event) =>{
-        input.value = input.value.trim();
 
-        if(event.key === 'Backspace')
-        {
-            if(input.value.length === 0  && index !== 0)
-            {
-                inputs[index - 1].value = "";
-                inputs[index - 1].focus();
+    inputs.forEach((input, index, arr) => {
+        input.addEventListener("input", () => {
+            if (index !== arr.length - 1) {
+                inputs[index + 1].focus();
             }
-        }
+        });
+
+        input.addEventListener("keyup", (event) => {
+            input.value = input.value.trim();
+
+            if (event.key === 'Backspace') {
+                if (input.value.length === 0 && index !== 0) {
+                    inputs[index - 1].value = "";
+                    inputs[index - 1].focus();
+                }
+            }
+            else if (event.key === 'Enter') {
+                verifyAccount();
+            }
+        });
     });
+    
+    function verifyValue() {
+        const value = inputs.reduce((result, input) => result + input.value.trim(), "");
+
+        if (value.length !== inputs.length) {
+            return false;
+        }
+        return true;
+    }
 
 
+    function verifyAccount() {
+        if (!verifyValue()) {
+            alert("Digite o código completo!");
+            return;
+        }
+
+        alert("Passou pelas verificações!");
+
+    }
 });
+
+
+
+
+
 
 
 
