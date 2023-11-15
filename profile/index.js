d84a12ad426fd305fdd871342d7f63c708674579
Leandro Cruz <LeandroCruz717@outlook.com>
2023-11-15 19:24:56 -0300

    feat: atualizar página profile

--- profile/index.js
+++ profile/index.js
@@ -1,16 +1,18 @@
-const ProfileDescription_display = document.getElementById("BioTextArea");
-ProfileDescription_display.setAttribute("disabled", "disabled");
+const profileDescriptionDisplay = document.getElementById('BioTextArea');
+
+profileDescriptionDisplay.setAttribute('disabled', 'disabled');
 
 function showProfileDescription() {
-  document.querySelector(".section__div--buttons").style.display = "flex";
-  ProfileDescription_display.removeAttribute("disabled", "");
+    document.querySelector('.section__div--buttons').style.display = 'flex';
+    profileDescriptionDisplay.removeAttribute('disabled', '');
 }
 
-document.querySelector("button#buttonSave").addEventListener("click", function () {
-  document.querySelector(".section__div--buttons").style.display = "none";
-  ProfileDescription_display.setAttribute("disabled", "disabled");
+document.querySelector('button#buttonSave').addEventListener('click', function () {
+    document.querySelector('.section__div--buttons').style.display = 'none';
+    profileDescriptionDisplay.setAttribute('disabled', 'disabled');
 });
-document.querySelector("button#buttonCancel").addEventListener("click", function () {
-  document.querySelector(".section__div--buttons").style.display = "none";
-  ProfileDescription_display.setAttribute("disabled", "disabled");
+
+document.querySelector('button#buttonCancel').addEventListener('click', function () {
+    document.querySelector('.section__div--buttons').style.display = 'none';
+    profileDescriptionDisplay.setAttribute('disabled', 'disabled');
 });
