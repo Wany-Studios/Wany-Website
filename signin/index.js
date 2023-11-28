942eee4677037551e59504ab6fc592130e010a6d
Leandro Cruz <LeandroCruz717@outlook.com>
2023-11-28 00:59:11 -0300

    feat(signin): implement reset password

--- signin/index.js
+++ signin/index.js
@@ -1,6 +1,6 @@
 let makingRequest = false;
 
-document.querySelector('form').addEventListener('submit', async function (e) {
+document.querySelector("form").addEventListener("submit", async function (e) {
     e.preventDefault();
 
     if (makingRequest) return;
@@ -13,7 +13,7 @@ document.querySelector('form').addEventListener('submit', async function (e) {
     if (!validateData(data)) return;
 
     makingRequest = true;
-    document.body.classList.add('waiting');
+    document.body.classList.add("waiting");
 
     const response = await signIn(data).catch(({ response }) => {
         const { data } = response;
@@ -23,10 +23,10 @@ document.querySelector('form').addEventListener('submit', async function (e) {
 
     if (response) {
         await saveLocalUser();
-        window.location.href = resolveUrl() + 'home/';
+        window.location.href = resolveUrl() + "home/";
     }
 
-    document.body.classList.remove('waiting');
+    document.body.classList.remove("waiting");
     makingRequest = false;
 });
 
@@ -35,9 +35,40 @@ function validateData(data) {
     return true;
 }
 
+async function forgotPassword() {
+    const email = document.getElementById("username-email");
+
+    if (!email.value.length) {
+        showErrorToForm("Please, enter your email address.");
+        email.focus();
+        return;
+    }
+
+    if (!email.value.includes("@")) {
+        showErrorToForm("You need to provide a valid email.");
+        email.focus();
+        return;
+    }
+
+    showErrorToForm();
+
+    try {
+        document.body.classList.add("waiting");
+        await requestResetPassword(email.value);
+        email.value = "";
+        alert("Reset password email has been sent. Check your inbox.");
+    } catch (err) {
+        showErrorToForm(
+            err.response?.data?.message || "Unable to request reset password."
+        );
+    } finally {
+        document.body.classList.remove("waiting");
+    }
+}
+
 function getFormData() {
-    const usernameOrEmail = document.getElementById('username-email').value;
-    const password = document.getElementById('password').value;
+    const usernameOrEmail = document.getElementById("username-email").value;
+    const password = document.getElementById("password").value;
 
     return {
         usernameOrEmail,
